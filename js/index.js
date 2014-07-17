df6c46536086e1eef952fa49927dced0c1428445
Yong Qian, Lee <b92101007@gmail.com>
2014-07-18 02:14:54 +0800

    移植meteor至cordova下

--- js/index.js
+++ js/index.js
@@ -1,32 +1,25 @@
 DB = {};
-DB.news = new Meteor.Collection('news');
-Meteor.subscribe('news');
+DB.news = new Meteor.Collection('News');
+Meteor.subscribe('unAssignNews');
 
 
 Meteor.startup(function() {
-  var $news = $('#news');
 
-  $news.on('click', 'a', function() {
-    var $this  = $(this)
-      , id     = $this.closest('p').attr('data-id')
-      , act    = $this.attr('data-act')
-      , update = {}
-      ;
-    update[ act ] = 1;
-    DB.news.update(id, {'$inc' : update});
+  Template.load('test', 'template/test.html').done(function() {
+    Template.test.helpers(
+      {'list' :
+          function() {
+            return [
+              {'name' : '測試一'}
+            , {'name' : '測試二'}
+            , {'name' : '測試三'}
+            , {'name' : '測試四'}
+            , {'name' : '測試五'}
+            ]
+          }
+      }
+    )
+    
   });
-
-Deps.autorun(function () {
-  var news = DB.news.find({}, {'sort' : {'error' : -1} })
-    , html = ''
-    ;
-
-  news.forEach(function(doc) {
-    html += '<li>' + doc.title + '</li>';
-    html += '<p>違：' + doc.error + '，爆：' + doc.hot + '，打：' + doc.strike + '，評：' + doc.comment + '。</p>';
-    html += '<p data-id="' + doc._id + '"><a href="javascript:" data-act="error">違</a><a href="javascript:" data-act="hot">爆</a><a href="javascript:" data-act="strike">打</a><a href="javascript:" data-act="comment">評</a></p>';
-  });
-  $('#news').html( html );
-});
 
 });
